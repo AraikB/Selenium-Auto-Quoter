4d76005d1a71634e6509c11bae0e4bbf4ecfc549
Araik Barouni <Araikb@gmail.com>
2020-11-11 21:55:36 +0000

    Corrected formatting on CSV Reader

--- src/com/readertut/CsvReaderWriter.java
+++ src/com/readertut/CsvReaderWriter.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReaderWriter {
-	//private 
+ 
 
 	private driverInfo driverInfo;
 
@@ -21,58 +21,9 @@ public class CsvReaderWriter {
 
 	public static void main(String[] args) {
 		String filePath = "C:\\Users\\Disco Ric\\Documents\\Book1.csv";
-
-		//	System.out.println("starting write driverInfo.csv file:" + filePath);
-		//	writeCsv(filePath);
-
 		System.out.println("Starting Read driverInfo.csv file");
-		//readCsv(filePath);
 	}
-	/*	public static void writeCsv(String filePath) {
-		List<driverInfo> driverInfos = new ArrayList<driverInfo>();
 
-		driverInfo driverInfo = new driverInfo();
-		driverInfo.setFirstName("Jack");
-		driverInfo.setLastName("Tutorial 1");;
-		driverInfos.add(driverInfo);
-
-		driverInfo = new driverInfo();
-		driverInfo.setId(2);
-		driverInfo.setFirstName("Jack");
-		driverInfo.setLastName("Tutorial 2");;
-		driverInfos.add(driverInfo);
-
-		driverInfo = new driverInfo();
-		driverInfo.setId(3);
-		driverInfo.setFirstName("Jack");
-		driverInfo.setLastName("Tutorial 3");;
-		driverInfos.add(driverInfo);
-
-		FileWriter fileWriter = null;
-		try {
-			fileWriter = new FileWriter(filePath);
-
-			fileWriter.append("Id, First Name, Last Name");
-			for(driverInfo u: driverInfos) {
-				fileWriter.append(String.valueOf(u.getId()));
-				fileWriter.append(",");
-				fileWriter.append(u.getFirstName());
-				fileWriter.append(",");
-				fileWriter.append(u.getLastName());
-				fileWriter.append("\n");
-
-				}
-		}catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			try {
-			fileWriter.flush();
-			fileWriter.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}*/
 	public driverInfo readCsv(String filePath) {
 		BufferedReader reader = null; 
 		driverInfo = new driverInfo();
@@ -105,11 +56,6 @@ public class CsvReaderWriter {
 					driverInfos.add(driverInfo);
 				}
 				for(driverInfo u: driverInfos) {
-					/*		System.out.printf("Client Name :%s, %s  \n Address : %s Zip Code : %s \n Phone Number - Email : %s - %s \n Residence Type : %s \n "
-							+ "Insured with Mercury : %s \n Date of Birth : %s \n Marital Status : %s \n License Nummber : %s \n Relationship to Insured : %s \n",  
-							u.getLastName(), u.getFirstName(), u.getMailingAddress(), 
-							u.getZipCode(), u.getPhoneNumber(), u.getEmailAddress(), u.getResidenceType(), u.getInsuredMerc(), u.dateOfBirth, u.getMaritalStatus(), 
-							u.getLicenseNumber(), u.getRelationtoInsured()); */
 				}
 			}
 		} catch (Exception ex) {
